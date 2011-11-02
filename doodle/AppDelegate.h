ddc246ff084aba90f34877b92545131a16402421
T. Andrew Binkowski <abinkowski@uchicago.edu>
2011-11-01 21:48:42 -0500

    Initial Commit

---
+++ doodle/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  doodle
+//
+//  Created by T. Andrew Binkowski on 11/1/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
